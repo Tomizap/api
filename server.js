ed0c1a885ed808a861b2f882cb715b631128edfb
Tom ZAPICO <zaptom.pro@gmail.com>
2024-03-03 21:41:49 +0100

    sync

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require("./src/app");
-const port = 7000;
+const port = 3000;
 
 // app.listen()
 app.listen(port, () => {
-  console.log(`App running on : http://localhost:${port}`);
+  console.log(`App running on : http://13.53.42.89:${port}`);
 });
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const routes = express.Router();
-const mongo = require("../db/mongo.js");
+// const mongo = require("../db/mongo.js");
 
 // HOME
 routes.get("/", (req, res) => res.send("Bienvenue sur l'api"));
@@ -8,71 +8,71 @@ routes.get("/", (req, res) => res.send("Bienvenue sur l'api"));
 // REGISTER
 
 // -------------------- AUTHENTIFICATE REQUEST ---------------------
-routes.use(require("../middleware/auth.js"));
+// routes.use(require("../middleware/auth.js"));
 
-// Interact with a mongo
-routes.post("/", async (req, res) => {
-  // console.log(req.body);
-  req.response.data = await mongo(req.body);
-  // console.log(req.response.data);
-  req.response.ok = 
-    !req.response.data || 
-    req.response.data.acknowledged || 
-    req.response.data.length > 0 ? true : false;
-  req.response.message = req.response.ok || req.response.data.acknowledged ? 
-    "operation success" : 
-    "operation fail";
-  req.response.config = req.body
-  return res.json(req.response);
-});
-
-// GET USER
-routes.get(["/me", '/auth'], (req, res) => {
-  res.json({
-    ok: true,
-    message: 'authentifaction réussie !',
-    user: req.user,
-  });
-});
-
-// OAuth
-routes.use("/oauth", require("./oauth.js"));
-
-// Google
-routes.use("/google", require("./google.js"));
-
-// Emailing
-routes.use("/email", require("./email.js"));
-
-// Download file from data
-routes.use("/download", require("./downloads.js"));
-
-// Get documents of mongo collection
-// routes.get("/mongo/:db/:collection", async (req, res) => {
-//   req.body.db = req.params.db 
-//   req.body.collection = req.params.collection 
-//   req.body.selector = req.query
-
-//   req.response.data = await mongo({
-//     db: req.params.db,
-//     collection: req.params.collection,
-//     selector: req.query
-//   });
-//   req.response.ok = req.response.data.length > 0 ? true : false;
-//   req.response.message = req.response.ok ? "" : "Aucune ressource trouvée";
+// // Interact with a mongo
+// routes.post("/", async (req, res) => {
+//   // console.log(req.body);
+//   req.response.data = await mongo(req.body);
+//   // console.log(req.response.data);
+//   req.response.ok = 
+//     !req.response.data || 
+//     req.response.data.acknowledged || 
+//     req.response.data.length > 0 ? true : false;
+//   req.response.message = req.response.ok || req.response.data.acknowledged ? 
+//     "operation success" : 
+//     "operation fail";
+//   req.response.config = req.body
 //   return res.json(req.response);
 // });
 
-// Appointments
-routes.use("/appointments", require("./appointments.js"));
+// // GET USER
+// routes.get(["/me", '/auth'], (req, res) => {
+//   res.json({
+//     ok: true,
+//     message: 'authentifaction réussie !',
+//     user: req.user,
+//   });
+// });
 
-// Schemas
-routes.use("/schemas", require("./schemas.js"));
+// // OAuth
+// routes.use("/oauth", require("./oauth.js"));
 
-// Automnations
-routes.use("/automnations", require("./automnations.js"));
+// // Google
+// routes.use("/google", require("./google.js"));
 
-// Contacts
-routes.use("/contacts", require("./contacts.js"));
+// // Emailing
+// routes.use("/email", require("./email.js"));
+
+// // Download file from data
+// routes.use("/download", require("./downloads.js"));
+
+// // Get documents of mongo collection
+// // routes.get("/mongo/:db/:collection", async (req, res) => {
+// //   req.body.db = req.params.db 
+// //   req.body.collection = req.params.collection 
+// //   req.body.selector = req.query
+
+// //   req.response.data = await mongo({
+// //     db: req.params.db,
+// //     collection: req.params.collection,
+// //     selector: req.query
+// //   });
+// //   req.response.ok = req.response.data.length > 0 ? true : false;
+// //   req.response.message = req.response.ok ? "" : "Aucune ressource trouvée";
+// //   return res.json(req.response);
+// // });
+
+// // Appointments
+// routes.use("/appointments", require("./appointments.js"));
+
+// // Schemas
+// routes.use("/schemas", require("./schemas.js"));
+
+// // Automnations
+// routes.use("/automnations", require("./automnations.js"));
+
+// // Contacts
+// routes.use("/contacts", require("./contacts.js"));
 
 module.exports = routes;
