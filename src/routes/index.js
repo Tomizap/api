a9eaa0b8d1eae024d71a6509db82a0a06479dc78
Tom ZAPICO <zaptom.pro@gmail.com>
2024-03-03 21:47:39 +0100

    sync

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const routes = express.Router();
-// const mongo = require("../db/mongo.js");
+const mongo = require("../db/mongo.js");
 
 // HOME
 routes.get("/", (req, res) => res.send("Bienvenue sur l'api"));
@@ -8,32 +8,32 @@ routes.get("/", (req, res) => res.send("Bienvenue sur l'api"));
 // REGISTER
 
 // -------------------- AUTHENTIFICATE REQUEST ---------------------
-// routes.use(require("../middleware/auth.js"));
+routes.use(require("../middleware/auth.js"));
 
-// // Interact with a mongo
-// routes.post("/", async (req, res) => {
-//   // console.log(req.body);
-//   req.response.data = await mongo(req.body);
-//   // console.log(req.response.data);
-//   req.response.ok = 
-//     !req.response.data || 
-//     req.response.data.acknowledged || 
-//     req.response.data.length > 0 ? true : false;
-//   req.response.message = req.response.ok || req.response.data.acknowledged ? 
-//     "operation success" : 
-//     "operation fail";
-//   req.response.config = req.body
-//   return res.json(req.response);
-// });
+// Interact with mongodb
+routes.post("/", async (req, res) => {
+  // console.log(req.body);
+  req.response.data = await mongo(req.body);
+  // console.log(req.response.data);
+  req.response.ok = 
+    !req.response.data || 
+    req.response.data.acknowledged || 
+    req.response.data.length > 0 ? true : false;
+  req.response.message = req.response.ok || req.response.data.acknowledged ? 
+    "operation success" : 
+    "operation fail";
+  req.response.config = req.body
+  return res.json(req.response);
+});
 
-// // GET USER
-// routes.get(["/me", '/auth'], (req, res) => {
-//   res.json({
-//     ok: true,
-//     message: 'authentifaction réussie !',
-//     user: req.user,
-//   });
-// });
+// GET USER
+routes.get(["/me", '/auth'], (req, res) => {
+  res.json({
+    ok: true,
+    message: 'authentifaction réussie !',
+    user: req.user,
+  });
+});
 
 // // OAuth
 // routes.use("/oauth", require("./oauth.js"));
