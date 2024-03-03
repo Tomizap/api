6926002817aa4610266a7b87b3cd827e7f55033d
Tom ZAPICO <zaptom.pro@gmail.com>
2024-03-03 21:52:41 +0100

    sync

--- src/routes/index.js
+++ src/routes/index.js
@@ -35,44 +35,28 @@ routes.get(["/me", '/auth'], (req, res) => {
   });
 });
 
-// // OAuth
-// routes.use("/oauth", require("./oauth.js"));
+// OAuth
+routes.use("/oauth", require("./oauth.js"));
 
-// // Google
-// routes.use("/google", require("./google.js"));
+// Google
+routes.use("/google", require("./google.js"));
 
-// // Emailing
-// routes.use("/email", require("./email.js"));
+// Emailing
+routes.use("/email", require("./email.js"));
 
-// // Download file from data
-// routes.use("/download", require("./downloads.js"));
+// Download file from data
+routes.use("/download", require("./downloads.js"));
 
-// // Get documents of mongo collection
-// // routes.get("/mongo/:db/:collection", async (req, res) => {
-// //   req.body.db = req.params.db 
-// //   req.body.collection = req.params.collection 
-// //   req.body.selector = req.query
-
-// //   req.response.data = await mongo({
-// //     db: req.params.db,
-// //     collection: req.params.collection,
-// //     selector: req.query
-// //   });
-// //   req.response.ok = req.response.data.length > 0 ? true : false;
-// //   req.response.message = req.response.ok ? "" : "Aucune ressource trouvée";
-// //   return res.json(req.response);
-// // });
-
-// // Appointments
+// Appointments
 // routes.use("/appointments", require("./appointments.js"));
 
-// // Schemas
+// Schemas
 // routes.use("/schemas", require("./schemas.js"));
 
-// // Automnations
+// Automnations
 // routes.use("/automnations", require("./automnations.js"));
 
-// // Contacts
-// routes.use("/contacts", require("./contacts.js"));
+// Contacts
+routes.use("/contacts", require("./contacts.js"));
 
 module.exports = routes;
