05856ef8205bbe9b2323b450c572cb4f1b8521b6
Tom ZAPICO <zaptom.pro@gmail.com>
2024-03-03 21:50:17 +0100

    sync

--- src/router.js
+++ src/router.js
@@ -2,16 +2,16 @@ const express = require("express");
 const router = express.Router();
 const routes = require("./routes/index.js");
 
-// router.use((req, res, next) => {
-//   req.api = require('./api.js')
-//   req.response = {
-//     ok: false,
-//     message: "",
-//     user: {},
-//     data: [],
-//   };
-//   next();
-// });
+router.use((req, res, next) => {
+  req.api = require('./api.js')
+  req.response = {
+    ok: false,
+    message: "",
+    user: {},
+    data: [],
+  };
+  next();
+});
 
 router.use(routes);
 
