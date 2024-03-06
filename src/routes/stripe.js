6a97cc84f134f4eda8eda27d0b45f693b36ae339
Tom ZAPICO <zaptom.pro@gmail.com>
2024-03-06 23:16:52 +0100

    sync

--- src/routes/stripe.js
+++ src/routes/stripe.js
@@ -2,6 +2,6 @@ const express = require("express");
 // const mongo = require("../db/mongo");
 const router = express.Router();
 
-
+router.get('/', (req, res) => {res.send('stripe')})
 
 module.exports = router
